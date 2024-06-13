da38efac651f1d46df1f1b2d4876d6c0374df65e
ivandjoh <ivandi.djoh@gmail.com>
2024-06-13 17:38:19 +0800

    major update!

--- lib/currencies.ts
+++ lib/currencies.ts
@@ -1,8 +1,6 @@
 export const Currencies = [
   { value: "USD", label: "$ Dollar", locale: "en-US" },
-  { value: "EUR", label: "€ Euro", locale: "de-DE" },
-  { value: "JPY", label: "¥ Yen", locale: "ja-JP" },
-  { value: "GBP", label: "£ Pound", locale: "en-GB" },
+  { value: "IDR", label: "Rp Rupiah", locale: "id-ID" },
 ];
 
 export type Currency = (typeof Currencies)[0];
